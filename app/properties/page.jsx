e7e462bab519ca44866b0708bcecb71b56a5f5ef
Mykola Akimov <mykola.akimov@bs2.lt>
2024-03-25 15:48:50 +0400

    properties list

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,11 +1,21 @@
-import Link from "next/link";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl">Properties Page</h1>
-      <Link href="/">Go home</Link>
-    </div>
+    <section class="px-4 py-6">
+      <div class="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
