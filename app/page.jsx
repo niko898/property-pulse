c3777f013f47208ef69dfde10e8393d1ca14a5dc
Mykola Akimov <mykola.akimov@bs2.lt>
2024-03-25 16:37:09 +0400

    added mongo

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 
-const HomePage = () => {
+const HomePage = async () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
